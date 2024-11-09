6ea701aa4e361668eab8f3331ed4e8aa282aad5e
MelAlejandrino <alejandrinomel02@gmail.com>
2024-11-09 14:47:45 +0800

    user website 2

--- frontend/user-website/postcss.config.js
+++ frontend/user-website/postcss.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    // other PostCSS plugins can be added here
+  },
+};
