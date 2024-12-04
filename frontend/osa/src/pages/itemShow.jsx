ea6c1ed9d30c10efd60a975b847e36217cd46ed7
Jefferson Sabejon <jepoysabejon24@gmail.com>
2024-12-05 00:11:47 +0800

    A LOT OF CHANGES

---
+++ frontend/osa/src/pages/itemShow.jsx
@@ -0,0 +1,25 @@
+import Sidebar from "../components/sidebar";
+import Topbar from "../components/topBar";
+import data from "../assets/data.json";
+
+export default function ItemShow() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="w-full">
+        <Topbar />
+        <main className="flex flex-col items-center p-5">
+          <h1 className="font-extrabold text-6xl">LOST ITEMS</h1>
+          <ul className="flex gap-4">
+            {data.map((item) => (
+              <li key={item.id} className="p-5 border-4 border-red-950">
+                <h2>{item.name}</h2>
+                <p>{item.description}</p>
+              </li>
+            ))}
+          </ul>
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/osa/src/pages/login.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"; 
+import { useAuth } from "../AuthContext"; // Import the custom hook
+
+export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate(); 
+  const { login, user } = useAuth(); // Get the login function and user state from context
+
+  // Check if user is already logged in
+  useEffect(() => {
+    if (user) {
+      navigate('/dashboard'); // Redirect to dashboard if user is logged in
+    }
+  }, [user, navigate]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/api/admin', {
+        username,
+        password,
+      });
+
+      console.log("User logged in successfully:", response.data);
+      login({ username }); // Call login function with user data
+      navigate('/dashboard'); 
+    } catch (error) {
+      setError("Invalid username or password");
+      console.error("Error logging in:", error.message);
+    }
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center bg-gradient-to-br from-blue-500 to-cyan-400">
+      <div className="bg-white p-10 rounded-lg shadow-lg max-w-xs w-full text-center">
+        <h2 className="mb-6 text-2xl text-gray-800 font-semibold">ADMIN LOGIN</h2>
+
+        <form onSubmit={handleLogin}>
+          {error && <p className="text-red-500">{error}</p>}
+          <div className="mb-4 text-left">
+            <label htmlFor="username" className="block mb-2 text-sm font-bold text-gray-700">Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              placeholder="Enter your username"
+              required
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="mb-4 text-left">
+            <label htmlFor="password" className="block mb-2 text-sm font-bold text-gray-700">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              placeholder="Enter your password"
+              required
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="mb-4 text-right">
+            <a href="#" className="text-sm text-blue-500 hover:underline">Forgot Password?</a>
+          </div>
+
+          <button type="submit" className="w-full py-3 bg-blue-500 text-white rounded hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500">LOGIN</button>
+        </form>
+      </div>
+    </div>
+  );
+};
