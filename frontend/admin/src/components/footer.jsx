ea12dd2e8b2049a0008d1b000bd1806a74041e46
Mel Alejandrino <alejandrinomel02@gmail.com>
2025-03-27 20:11:00 +0800

    Add Dockerfiles for backend and frontend

--- frontend/admin/src/components/footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <div className="p-2 bg-blue-950 text-white flex items-center w-auto">
-        <p className="text-1xl text-center"> © U-Find Philippines 2024 </p>
-      </div>
-    );
-  }
-  
--- frontend/osa/src/components/topBar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Topbar() {
-  return (
-    <div className="p-5 bg-blue-950 text-white flex items-center w-auto">
-      <h1 className="text-3xl font-extrabold">U-FIND</h1>
-    </div>
-  );
-}
